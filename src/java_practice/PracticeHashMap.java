3d24944d9348fb9a027bfee93f75ec36132b4c99
Lin Fei Kang <linfeikang123@gmail.com>
2022-03-22 19:31:08 -0400

    feat: Practiced more functions and added comments.

--- src/java_practice/PracticeHashMap.java
+++ src/java_practice/PracticeHashMap.java
@@ -7,15 +7,30 @@ public class PracticeHashMap {
 	public static void main(String[] args) {
 		HashMap<String, Integer> ages = new HashMap<String, Integer>();
 		
-		// key, value
+		// putting an item with key, value
 		ages.put("Amy", 31);
 		ages.put("Jake", 33);
+		ages.put("Jake", 25); // keys are unique, this overrides the previous value
 		ages.put("Rosa", 32);
 		
+		// size
+		System.out.println(ages.size()); // 3
+		
 		for (String person : ages.keySet()) {
+			// accessing an item
 			System.out.println(person + " is " + ages.get(person) + " years old.");
 		}
-
+		
+		// values
+		System.out.println(ages.values());
+		
+		// removing
+		ages.remove("Jake");
+		System.out.println(ages);
+		
+		// removing all
+		ages.clear();
+		System.out.println(ages);
 	}
 
 }
