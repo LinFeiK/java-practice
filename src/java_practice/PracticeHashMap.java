9c150cb3adbb6a317a6ce0999f1875d3a510452a
Lin Fei Kang <linfeikang123@gmail.com>
2022-03-21 23:33:17 -0400

    feat: Created HashMap object, put keys and values, and used a for loop on the keySet

--- src/java_practice/PracticeHashMap.java
+++ src/java_practice/PracticeHashMap.java
@@ -1,9 +1,20 @@
 package java_practice;
 
+import java.util.HashMap;
+
 public class PracticeHashMap {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		HashMap<String, Integer> ages = new HashMap<String, Integer>();
+		
+		// key, value
+		ages.put("Amy", 31);
+		ages.put("Jake", 33);
+		ages.put("Rosa", 32);
+		
+		for (String person : ages.keySet()) {
+			System.out.println(person + " is " + ages.get(person) + " years old.");
+		}
 
 	}
 
