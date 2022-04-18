ad9869659914a6af975d5b39f0243832258d03aa
Lin Fei Kang <linfeikang123@gmail.com>
2022-04-17 23:20:19 -0400

    chore: fixed error in base cases for leastCommonMultiple

--- src/java_practice/GCFandLCM.java
+++ src/java_practice/GCFandLCM.java
@@ -32,8 +32,8 @@ public class GCFandLCM {
     }
 	
 	public static int leastCommonMultiple(int a, int b) {
-		if (a % b == 0) return b;
-		if (b % a == 0) return a;
+		if (a % b == 0) return a;
+		if (b % a == 0) return b;
 		return a / greatestCommonFactor(a, b) * b;
 	}
 
